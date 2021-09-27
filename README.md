c17b7e0fd08ae670bbee671b1f2d733eea73d7b6
Abhinav Kulkarni <abhivbk13@gmail.com>
2021-09-27 11:39:34 +0530

    Initial COmmit

---
+++ README.md
@@ -0,0 +1,81 @@
+
+# Em'SHOCK
+
+### 🎵 **MUSIC** 🎵
+
+- Do you Love to hear music? 🎧
+
+- Do you love to Dance? 🕺💃
+
+- Do you hear music while Gaming? 🎮
+
+---
+Why do it alone when you can Just join on DISCORD and Invite [**Em'SHOCK**](https://discord.com/api/oauth2/authorize?client_id=774975725610205214&permissions=8&scope=bot)
+It is Bot which Would play Music for you, your friends, relatives!
+---
+While you just keep Grooving, Dancing and Enjoying
+while **Em'Shock** Plays Songs for you.
+
+
+
+
+
+## Requirements
+
+#### Discord should be installed on your
+- Windows
+- Mac
+- Linux
+- Android
+- IOS
+
+---
+#### You should be the Owner👑 of the server or should have the administrative controls of the server.
+- As your bot takes the administrative controls of your server.
+- It will not mess with your server. Will just work on your command.
+## Installation
+
+- Go to this [Website](https://discord.com/api/oauth2/authorize?client_id=774975725610205214&permissions=8&scope=bot) and Login with your discord account
+
+![login](https://1.bp.blogspot.com/-b0tFCZTmIRs/YURr3SSVlFI/AAAAAAAAvpE/AgcGIdXejAgYj7YdCyRLFvvJYBWptJS_gCLcBGAsYHQ/s1173/1%2BLogin%2BPAge.png)
+
+- After Loging in choose a server.
+
+![Choose a server](https://1.bp.blogspot.com/-uMEfZMNtGHc/YURr3dIdW_I/AAAAAAAAvpA/BKJ46_RRxQcmnHpKAHO4YXFtVAnZjJ5GQCLcBGAsYHQ/s652/3%2BChoose%2Ba%2BServer.png)
+
+- Authorize the bot and give it administrator permissions for your server
+
+![Authorization](https://1.bp.blogspot.com/-CemP7R6pTq8/YURr4VbM6RI/AAAAAAAAvpM/iCaLNyczmHooU1GgLQQfG-eCkeMFX-uPwCLcBGAsYHQ/s901/4%2BAuthorization.png)
+
+- Now you are all set to go! Just Sit Back And Enjoy With **Em'Shock**.
+
+![done](https://1.bp.blogspot.com/-Y-XXvPxDutE/YURr4tKFD5I/AAAAAAAAvpQ/Yxrai183p-E5yJS8HUnDzJSPMugQkhSZwCLcBGAsYHQ/s595/5%2BAuthorized.png)
+
+## Em'Shock commands
+
+#### Command format
+
+```python
+    ![Command]
+```
+
+| Command | Function   | 
+| :-------- | :------- |
+| `!play[song_name]` | `Join's your voice channel. Searches for the song and plays it.` |
+| `!pause`           |`Pauses the currently playing song`                              |
+|`!resume`           |`Resumes the song`                                               |
+|`!stop`             |`Stops the cuurently playing song`                               |
+|`!leave`            |`At the end of you session you can ask Em'SHOCK to leave the voice channel.`|
+
+
+
+  # PEREGRINE INC.
+![Logo](https://1.bp.blogspot.com/-dC6TUCpkl-4/YURr9GL3DLI/AAAAAAAAvpU/dhlskWO48KEiwA2cNRCULLx7P51svqnTQCLcBGAsYHQ/s1935/Logo.jpg)
+### Things Don't Change with the speed. We change Thing's With Our speed. 
+~Peregrine inc.
+## License
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+
+  
---
+++ main.py
@@ -0,0 +1,126 @@
+import discord
+from discord.ext import commands
+import youtube_dl
+import os
+from youtubesearchpython import VideosSearch
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+bot = commands.Bot(command_prefix="!")
+
+youtube_dl.utils.bug_reports_message = lambda:''
+
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is Online and is in the following SERVERS \n {bot.guilds}\n")
+
+@bot.command()
+async def play(ctx, song_name:str):
+    #joining voice channel
+    try:
+        if not ctx.message.author.voice:
+            await ctx.send(f"{ctx.message.author.name} is not connected to a voice channel")
+            return
+        else:
+            channel = ctx.message.author.voice.channel
+            print(channel)
+        await channel.connect()
+
+    except Exception as e:
+        print(e)
+
+    #downloadind the song
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3") # If already song is downloaded delete it
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+        return
+
+    voice = ctx.message.guild.voice_client
+
+    # Preferences for Youtube video download
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    await ctx.send(f"Searching... for :mag_right: {song_name}")
+
+    # Searching fot the song on youtube
+    videosSearch = VideosSearch(song_name, limit = 1)
+    results = str(videosSearch.result())
+    partial_url = results[results.index('https://www.youtube.com/watch?') + 1:]
+    partial_url = partial_url[:partial_url.index("'") ]
+    url = str('h' + partial_url)
+
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            for file in os.listdir("./"):
+                    if file.endswith(".mp3"):
+                        os.rename(file, "song.mp3")
+                        voice.play(discord.FFmpegPCMAudio("song.mp3"))
+                        await ctx.send(f'Playing {url}')
+
+    except Exception as e:
+        print(e)
+
+@bot.command(name='leave', help='To make the bot leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+        await ctx.send("OK leaving the Voice Channel")
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+@bot.command(name='pause', help='This command pauses the song')
+async def pause(ctx):
+    try:
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_playing():
+            await voice_client.pause()
+            await ctx.send("Song Paused. To resume the song just type '!resume'")
+        else:
+            await ctx.send("The bot is not playing anything at the moment.")
+    except Exception as e:
+        print(e)
+    
+@bot.command(name='resume', help='Resumes the song')
+async def resume(ctx):
+    try:
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_paused():
+            await voice_client.resume()
+            await ctx.send("Song resumed. To pause the song just type '!pause'")
+
+        else:
+            await ctx.send("The bot was not playing anything before this. Use play_song command")
+    except Exception as e:
+        print(e)
+
+@bot.command(name='stop', help='Stops the song')
+async def stop(ctx):
+    try:
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_playing():
+            await voice_client.stop()
+            await ctx.send("Song Stopped")
+        else:
+            await ctx.send("The bot is not playing anything at the moment.")
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__" :
+        bot.run(TOKEN)
+
